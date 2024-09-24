13d5b29cc47eae5c527757cfb679453d1008155f
Muhammad Umar <99128329+Umar563@users.noreply.github.com>
2024-09-24 18:55:36 +0500

    pushing code to repo

---
+++ basics-01/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code as a newer version
+
+//alert( 3+3); we are using node js not browser;
+
+console.log(3+3)
+console.log("umar")
+
+let name="umar";
+let age = 26
+let isLoggedIn=false
+let state;
+
+
+// number => 2  to power 53
+//bigint
+// string  =>" "
+//boolean => true /false
+//null => standalone value
+// undefine => state declare but not assign a value;
+//symbol => unique 
+
+
+// object 
+
+console.log(typeof "Umar")
+console.log(typeof null)
---
+++ basics-01/test.js
@@ -0,0 +1,3 @@
+console.log("hello")
+const accountId= 23;
+console.log(accountId)
