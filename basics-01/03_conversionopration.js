f20a3483dcc2c699f70814f62b6414ffc6557fa4
Muhammad Umar <99128329+Umar563@users.noreply.github.com>
2024-09-24 19:17:27 +0500

    conversion of datatype

---
+++ basics-01/03_conversionopration.js
@@ -0,0 +1,44 @@
+let score ="33"
+let score2 ="33abc"
+
+let score3= "true"
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+let valueInNumber2 = Number(score2)
+
+
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+let vlaueInNumber3 = Number(score3)
+
+console.log(typeof(vlaueInNumber3))
+console.log(vlaueInNumber3)
+
+// 33 => 33
+//"33abc" => NaN
+//true =>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true;0 => false
+// "" => false
+// "umar" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
